b4683bc7f3ba87382a8d8b46291b9466c1d69b42
Claudio Cruz <claudiocruz1989@gmail.com>
2022-12-11 17:27:16 +0000

    Functional componets and CSS moduleschallenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import css from "./css/Sidebar.module.css"
+
+
+const Sidebar = (props) => {
+    console.log(props);
+    return <div className={css.sidebar}>
+        <a href="#">MyPhotos</a>
+        <a href="#">My Illustrations</a>
+        <a href="#">My Paintings</a>
+    </div>
+}
+
+export default Sidebar;
