bfaab556811792ce4ff72bfdc186053134263035
Claudio Cruz <claudiocruz1989@gmail.com>
2022-12-18 12:54:03 +0000

    Component Lifecycles Challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
+import css from "../src/App.module.css"
 import Sidebar from "./components/Sidebar"
 // import NavBarSimple from "./components/NavBarSimple"
 import NavBarForm from "./components/NavBarForm"
 import Content from "./components/Content";
 
+
 function App() {
   return (
-    <div className="App">
+    <div className={css.App}>
     {/* <NavBarSimple /> */}
     <NavBarForm/>
     <Sidebar />
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,24 +2,42 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css"
 import {savedPosts} from "../posts.json"
 import PostItem from "./PostItem"
+import Loader from "./Loader"
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isLoaded: false,
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
     }
 
     render() {
         return (
 
             <div className={css.content}>
-
+                
                 <div className={css.TitleBar}>
                     <h1>My Posts</h1>
                 </div>
 
                 <div className={css.SearchResults}>
+                    {this.state.isLoaded ? 
                     <PostItem savedPosts={savedPosts} />
+                    :
+                    <Loader isLoaded={this.state.isLoaded}/>
+                    }
+                    
                 </div>
+                
             </div>
         )
     }
